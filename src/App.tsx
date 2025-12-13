63ba2ffb05ea435f8d06b558bfdd379f653989aa
Vadim Tokaev <fw.mbagg@gmail.com>
2025-12-13 15:13:21 +0300

    add periods panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import PeriodsPanel from "./components/PeriodsPanel/PeriodsPanel";
 import TotalsBlock from "./components/Totals/TotalsBlock";
 
 function App() {
     return (
         <div className="app">
-            <Header />
-            <TotalsBlock />
+            <div className="top_side">
+                <Header />
+                <TotalsBlock />
+            </div>
+            <div className="bottom_side">
+                <PeriodsPanel />
+            </div>
         </div>
     );
 }
